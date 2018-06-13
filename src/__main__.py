eaf1e2e37ac22ac9ea8596830b16bbfdcc2009ce
Sergio Pedri <sergio0694@live.com>
2018-06-14 00:54:16 +0200

    Removed additional test code

--- src/__main__.py
+++ src/__main__.py
@@ -8,11 +8,5 @@ os.environ['TF_MIN_GPU_MULTIPROCESSOR_COUNT'] = '6' # Enable all GPUs
 os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'      # See issue #152
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
-# validate test samples
-_, groups, _ = calculate_samples_data(TEST_DATASET_PATH, 1) # TODO: make the window size a macro too
-for group in groups:
-    _, _, error = calculate_image_difference('{}\\{}'.format(TEST_DATASET_PATH, group[0]), '{}\\{}'.format(TEST_DATASET_PATH, group[-1]))
-    assert IMAGE_DIFF_MIN_THRESHOLD < error < IMAGE_DIFF_MAX_THRESHOLD
-
 # debug run
 train.run()

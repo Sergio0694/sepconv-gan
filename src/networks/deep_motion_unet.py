95c5fd5d0ee5af3e8ba82923695e888764bd691d
Sergio Pedri <sergio0694@live.com>
2018-06-16 00:14:11 +0200

    Unet code refactored

--- src/networks/deep_motion_unet.py
+++ src/networks/deep_motion_unet.py
@@ -10,61 +10,58 @@ def get_network(x):
     x(tf.Tensor<tf.float32>) -- the input frames
     '''
 
-    x_stack = cnn.stack_images(x)
-    conv1_a = tf.layers.conv2d(x_stack, 32, 3, activation=tf.nn.leaky_relu, padding='same')
-    norm1_a = tf.layers.batch_normalization(conv1_a)
-    conv1_b = tf.layers.conv2d(norm1_a, 32, 3, activation=tf.nn.leaky_relu, padding='same')
-    norm1_b = tf.layers.batch_normalization(conv1_b)
-    pool1 = tf.layers.max_pooling2d(norm1_b, 2, 2, padding='same')
+    def downscale_block(tensor, filters):
+        down_conv1 = tf.layers.conv2d(tensor, filters, 3, activation=tf.nn.leaky_relu, padding='same')
+        down_norm1 = tf.layers.batch_normalization(down_conv1)
+        down_conv2 = tf.layers.conv2d(down_norm1, filters, 3, activation=tf.nn.leaky_relu, padding='same')
+        down_norm2 = tf.layers.batch_normalization(down_conv2)
+        down_pool = tf.layers.max_pooling2d(down_norm2, 2, 2, padding='same')
+        return down_norm2, down_pool
 
-    conv2_a = tf.layers.conv2d(pool1, 64, 3, activation=tf.nn.leaky_relu, padding='same')
-    norm2_a = tf.layers.batch_normalization(conv2_a)
-    conv2_b = tf.layers.conv2d(norm2_a, 64, 3, activation=tf.nn.leaky_relu, padding='same')
-    norm2_b = tf.layers.batch_normalization(conv2_b)
-    pool2 = tf.layers.max_pooling2d(norm2_b, 2, 2, padding='same')
+    def upscale_block(tensor, residue, filters):
+        up_shape = tf.shape(residue)
+        up_concat = tf.concat([tf.image.resize_bilinear(tensor, [up_shape[1], up_shape[2]]), residue], -1)
+        up_conv1 = tf.layers.conv2d(up_concat, filters, 3, activation=tf.nn.leaky_relu, padding='same')
+        up_norm1 = tf.layers.batch_normalization(up_conv1)
+        up_conv2 = tf.layers.conv2d(up_norm1, filters, 3, activation=tf.nn.leaky_relu, padding='same')
+        up_norm2 = tf.layers.batch_normalization(up_conv2)
+        return up_norm2
 
-    conv3_a = tf.layers.conv2d(pool2, 128, 3, activation=tf.nn.leaky_relu, padding='same')
-    norm3_a = tf.layers.batch_normalization(conv3_a)
-    conv3_b = tf.layers.conv2d(norm3_a, 128, 3, activation=tf.nn.leaky_relu, padding='same')
-    norm3_b = tf.layers.batch_normalization(conv3_b)
-    pool3 = tf.layers.max_pooling2d(norm3_b, 2, 2, padding='same')
+    with tf.variable_scope('unet', None, [x]):
 
-    conv4_a = tf.layers.conv2d(pool3, 256, 3, activation=tf.nn.leaky_relu, padding='same')
-    norm4_a = tf.layers.batch_normalization(conv4_a)
-    conv4_b = tf.layers.conv2d(norm4_a, 256, 3, activation=tf.nn.leaky_relu, padding='same')
-    norm4_b = tf.layers.batch_normalization(conv4_b)
-    pool4 = tf.layers.max_pooling2d(norm4_b, 2, 2, padding='same')
+        with tf.variable_scope('reshape', None, [x]):
+            x_stack = cnn.stack_images(x)
 
-    conv5_a = tf.layers.conv2d(pool4, 512, 3, activation=tf.nn.leaky_relu, padding='same')
-    norm5_a = tf.layers.batch_normalization(conv5_a)
-    conv5_b = tf.layers.conv2d(norm5_a, 512, 3, activation=tf.nn.leaky_relu, padding='same')
-    norm5_b = tf.layers.batch_normalization(conv5_b)
+        with tf.variable_scope('encoder', None, [x_stack]):
+            with tf.variable_scope('downscale_1', None, [x_stack]):
+                norm1, pool1 = downscale_block(x_stack, 32)
 
-    up6_s = tf.shape(conv4_b)
-    up6 = tf.concat([tf.image.resize_bilinear(norm5_b, [up6_s[1], up6_s[2]]), norm4_b], -1)
-    conv6_a = tf.layers.conv2d(up6, 256, 3, activation=tf.nn.leaky_relu, padding='same')
-    norm6_a = tf.layers.batch_normalization(conv6_a)
-    conv6_b = tf.layers.conv2d(norm6_a, 256, 3, activation=tf.nn.leaky_relu, padding='same')
-    norm6_b = tf.layers.batch_normalization(conv6_b)
+            with tf.variable_scope('downscale_2', None, [pool1]):
+                norm2, pool2 = downscale_block(pool1, 64)
 
-    up7_s = tf.shape(conv3_b)
-    up7 = tf.concat([tf.image.resize_bilinear(norm6_b, [up7_s[1], up7_s[2]]), norm3_b], -1)
-    conv7_a = tf.layers.conv2d(up7, 128, 3, activation=tf.nn.leaky_relu, padding='same')
-    norm7_a = tf.layers.batch_normalization(conv7_a)
-    conv7_b = tf.layers.conv2d(norm7_a, 128, 3, activation=tf.nn.leaky_relu, padding='same')
-    norm7_b = tf.layers.batch_normalization(conv7_b)
+            with tf.variable_scope('downscale_3', None, [pool2]):
+                norm3, pool3 = downscale_block(pool2, 128)
 
-    up8_s = tf.shape(conv2_b)
-    up8 = tf.concat([tf.image.resize_bilinear(norm7_b, [up8_s[1], up8_s[2]]), norm2_b], -1)
-    conv8_a = tf.layers.conv2d(up8, 64, 3, activation=tf.nn.leaky_relu, padding='same')
-    norm8_a = tf.layers.batch_normalization(conv8_a)
-    conv8_b = tf.layers.conv2d(norm8_a, 64, 3, activation=tf.nn.leaky_relu, padding='same')
-    norm8_b = tf.layers.batch_normalization(conv8_b)
+            with tf.variable_scope('downscale_4', None, [pool3]):
+                norm4, pool4 = downscale_block(pool3, 256)
 
-    up9_s = tf.shape(conv1_b)
-    up9 = tf.concat([tf.image.resize_bilinear(norm8_b, [up9_s[1], up9_s[2]]), norm1_b], -1)
-    conv9_a = tf.layers.conv2d(up9, 32, 3, activation=tf.nn.leaky_relu, padding='same')
-    norm9_a = tf.layers.batch_normalization(conv9_a)
-    conv9_b = tf.layers.conv2d(norm9_a, 32, 3, activation=tf.nn.leaky_relu, padding='same')
-    norm9_b = tf.layers.batch_normalization(conv9_b)
-    return tf.layers.conv2d(norm9_b, 3, 1, activation=tf.nn.sigmoid)
+            with tf.variable_scope('hidden_vector', None, [pool4]):
+                h_conv1 = tf.layers.conv2d(pool4, 512, 3, activation=tf.nn.leaky_relu, padding='same')
+                h_norm1 = tf.layers.batch_normalization(h_conv1)
+                h_conv2 = tf.layers.conv2d(h_norm1, 512, 3, activation=tf.nn.leaky_relu, padding='same')
+                h_norm2 = tf.layers.batch_normalization(h_conv2)
+
+        with tf.variable_scope('decoder', None, [h_norm2, norm4, norm3, norm2, norm1]):
+            with tf.variable_scope('upscale_1', None, [h_norm2, norm4]):
+                up4 = upscale_block(h_norm2, norm4, 256)
+
+            with tf.variable_scope('upscale_2', None, [up4, norm3]):
+                up3 = upscale_block(up4, norm3, 128)
+
+            with tf.variable_scope('upscale_3', None, [up3, norm2]):
+                up2 = upscale_block(up3, norm2, 64)
+
+            with tf.variable_scope('upscale_4', None, [up2, norm1]):
+                up1 = upscale_block(up2, norm1, 32)
+
+            return tf.layers.conv2d(up1, 3, 1, activation=tf.nn.sigmoid)

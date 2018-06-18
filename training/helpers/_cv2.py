329c8538125bc0adaa2ad579ee6ed2b159587702
Sergio Pedri <Sergio0694@live.com>
2018-06-18 20:21:34 +0200

    Added _cv file with optical flow method

---
+++ training/helpers/_cv2.py
@@ -0,0 +1,30 @@
+from enum import Enum
+import cv2
+
+class OpticalFlowType(Enum):
+    DIRECTIONAL = 0,
+    BIDIRECTIONAL = 1
+
+def get_optical_flow_rgb(before, after, flow_type):
+    '''Returns the appropriate optical flow estimation between two RB images.'''
+
+    # convert to grayscale
+    before_g = cv2.cvtColor(before, cv2.COLOR_BGR2GRAY)
+    after_g = cv2.cvtColor(after, cv2.COLOR_BGR2GRAY)
+    
+    if flow_type == OpticalFlowType.DIRECTIONAL:
+        return get_rgb_flow(before_g, after_g)
+    else:
+        return get_rgb_flow(before_g, after_g), get_rgb_flow(after_g, before_g)
+
+def get_rgb_flow(before, after):
+    '''Reads two grayscale images and returns the RGB optical flow between them.'''
+
+    flow = cv2.calcOpticalFlowFarneback(before_g, after_g, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+    magnitude, angle = cv2.cartToPolar(flow[:,:,0], flow[:,:,1])
+    hsv = np.zeros([before.shape[0], before.shape[1], 3])
+    hsv[:,:,0] = angle * 180 / np.pi / 2
+    hsv[:,:,1] = 255
+    hsv[:,:,2] = cv2.normalize(magnitude, None, 0, 255, cv2.NORM_MINMAX)
+    flow_bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+    return flow_bgr

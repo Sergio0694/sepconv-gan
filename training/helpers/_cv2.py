72910a0ede53e18b9f4157ccd5d86caa36244f36
Sergio Pedri <sergio0694@live.com>
2018-06-19 20:05:53 +0200

    Added bidirectional prewarp code

--- training/helpers/_cv2.py
+++ training/helpers/_cv2.py
@@ -2,9 +2,36 @@ from enum import Enum
 import cv2
 import numpy as np
 
-class OpticalFlowType(Enum):
+class OpticalFlowEmbeddingType(Enum):
     DIRECTIONAL = 0
     BIDIRECTIONAL = 1
+    BIDIRECTIONAL_PREWARPED = 2
+
+def prewarp_frame(flow, frame):
+    '''Warps a frame given its optical flow data.
+
+    flow(np.array) -- the optical flow data
+    frame(np.array) -- the image to warp
+    '''
+
+    h, w = flow.shape[:2]
+    flow = -flow
+    flow[:, :, 0] += np.arange(w)
+    flow[:, :, 1] += np.arange(h)[:, np.newaxis]
+    return cv2.remap(frame, flow, None, cv2.INTER_LINEAR)
+
+def get_bidirectional_prewarped_frames(before, after):
+    '''Returns a pair of warped frames, from A to B and from B to A.
+
+    before(np.array) -- the first frame
+    after(np.array) -- the second frame
+    '''
+
+    before_g = cv2.cvtColor(before, cv2.COLOR_BGR2GRAY)
+    after_g = cv2.cvtColor(after, cv2.COLOR_BGR2GRAY)
+    flow_forward = cv2.calcOpticalFlowFarneback(before_g, after_g, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+    flow_backward = cv2.calcOpticalFlowFarneback(after_g, before_g, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+    return prewarp_frame(flow_forward, before), prewarp_frame(flow_backward, after)
 
 def get_optical_flow_from_rgb(before, after, flow_type):
     '''Returns the appropriate optical flow estimation between two RB images.'''

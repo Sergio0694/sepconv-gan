0def1d9c27ce9389e44e5776f8112538880fff9c
Sergio Pedri <sergio0694@live.com>
2018-06-19 10:49:48 +0200

    Added variant of U-Net v2 network

--- training/networks/generators/deep_motion_unet.py
+++ training/networks/generators/deep_motion_unet.py
@@ -77,7 +77,7 @@ def get_network_v2(x):
         down_b1_conv = tf.layers.conv2d(down_b1_pool, 16, 1, activation=tf.nn.leaky_relu)
         down_b2_conv1 = tf.layers.conv2d(tensor, 32, 1, activation=tf.nn.leaky_relu)
         down_b2_norm1 = tf.layers.batch_normalization(down_b2_conv1)
-        down_b2_conv2 = tf.layers.conv2d(down_b2_norm1, filters // 2, 3, 1, activation=tf.nn.leaky_relu, padding='same')
+        down_b2_conv2 = tf.layers.conv2d(down_b2_norm1, filters // 2, 3, activation=tf.nn.leaky_relu, padding='same')
         down_b3_conv1 = tf.layers.conv2d(tensor, filters, 3, activation=tf.nn.leaky_relu, padding='same')
         down_b3_norm1 = tf.layers.batch_normalization(down_b3_conv1)
         down_b3_conv2 = tf.layers.conv2d(down_b3_norm1, filters, 3, activation=tf.nn.leaky_relu, padding='same')
@@ -133,6 +133,79 @@ def get_network_v2(x):
                 up1 = upscale_block(up2, norm1, 32)
 
             return tf.layers.separable_conv2d(up1, 3, 3, activation=tf.nn.sigmoid, padding='same')
+
+def get_network_v2_1(x):
+    '''An improved version of the network, with Inception-based downscale/upscale modules.
+
+    x(tf.Tensor<tf.float32>) -- the input frames
+    '''
+
+    def downscale_block(tensor, filters):
+        down_b1_pool = tf.layers.average_pooling2d(tensor, 2, 1, padding='same')
+        down_b1_conv = tf.layers.conv2d(down_b1_pool, 16, 1, activation=tf.nn.leaky_relu)
+        down_b2_conv1 = tf.layers.conv2d(tensor, 32, 1, activation=tf.nn.leaky_relu)
+        down_b2_norm1 = tf.layers.batch_normalization(down_b2_conv1)
+        down_b2_conv2 = tf.layers.conv2d(down_b2_norm1, filters // 2, 3, activation=tf.nn.leaky_relu, padding='same')
+        down_b3_conv1 = tf.layers.conv2d(tensor, filters, 3, activation=tf.nn.leaky_relu, padding='same')
+        down_b3_norm1 = tf.layers.batch_normalization(down_b3_conv1)
+        down_b3_conv2 = tf.layers.conv2d(down_b3_norm1, filters, 3, activation=tf.nn.leaky_relu, padding='same')
+        down_stack = tf.concat([down_b1_conv, down_b2_conv2, down_b3_conv2], -1)
+        down_norm = tf.layers.batch_normalization(down_stack)
+        down_pool = tf.layers.max_pooling2d(down_stack, 2, 2, padding='same')
+        return down_norm, down_pool
+
+    def upscale_block(tensor, residue, filters):
+        up_shape = tf.shape(residue)
+        up_concat = tf.concat([tf.image.resize_bilinear(tensor, [up_shape[1], up_shape[2]]), residue], -1)
+        up_b1_conv = tf.layers.conv2d(up_concat, 16, 1, activation=tf.nn.leaky_relu)
+        up_b2_conv1 = tf.layers.conv2d(up_concat, 32, 1, activation=tf.nn.leaky_relu)
+        up_b2_norm1 = tf.layers.batch_normalization(up_b2_conv1)
+        up_b2_conv2 = tf.layers.conv2d(up_b2_norm1, filters // 4, 3, activation=tf.nn.leaky_relu, padding='same')
+        up_b3_conv1 = tf.layers.conv2d(up_concat, filters, 3, activation=tf.nn.leaky_relu, padding='same')
+        up_b3_norm1 = tf.layers.batch_normalization(up_b3_conv1)
+        up_b3_conv2 = tf.layers.conv2d(up_b3_norm1, filters, 3, activation=tf.nn.leaky_relu, padding='same')
+        up_stack = tf.concat([up_b1_conv, up_b2_conv2, up_b3_conv2], -1)
+        up_norm = tf.layers.batch_normalization(up_stack)
+        return up_norm
+
+    with tf.variable_scope('unet', None, [x]):
+
+        with tf.variable_scope('reshape', None, [x]):
+            x_stack = _tf.stack_images(x)
+
+        with tf.variable_scope('encoder', None, [x_stack]):
+            with tf.variable_scope('downscale_1', None, [x_stack]):
+                norm1, pool1 = downscale_block(x_stack, 32)
+
+            with tf.variable_scope('downscale_2', None, [pool1]):
+                norm2, pool2 = downscale_block(pool1, 64)
+
+            with tf.variable_scope('downscale_3', None, [pool2]):
+                norm3, pool3 = downscale_block(pool2, 128)
+
+            with tf.variable_scope('downscale_4', None, [pool3]):
+                norm4, pool4 = downscale_block(pool3, 256)
+
+            with tf.variable_scope('hidden_vector', None, [pool4]):
+                h_conv1 = tf.layers.conv2d(pool4, 512, 3, activation=tf.nn.leaky_relu, padding='same')
+                h_norm1 = tf.layers.batch_normalization(h_conv1)
+                h_conv2 = tf.layers.conv2d(h_norm1, 512, 3, activation=tf.nn.leaky_relu, padding='same')
+                h_norm2 = tf.layers.batch_normalization(h_conv2)
+
+        with tf.variable_scope('decoder', None, [h_norm2, norm4, norm3, norm2, norm1]):
+            with tf.variable_scope('upscale_1', None, [h_norm2, norm4]):
+                up4 = upscale_block(h_norm2, norm4, 256)
+
+            with tf.variable_scope('upscale_2', None, [up4, norm3]):
+                up3 = upscale_block(up4, norm3, 128)
+
+            with tf.variable_scope('upscale_3', None, [up3, norm2]):
+                up2 = upscale_block(up3, norm2, 64)
+
+            with tf.variable_scope('upscale_4', None, [up2, norm1]):
+                up1 = upscale_block(up2, norm1, 32)
+
+            return tf.layers.conv2d(up1, 3, 3, activation=tf.nn.sigmoid, padding='same')
 
 def get_network_v3(x):
     '''A variant of the U-Net network, based on separable convolution layers.

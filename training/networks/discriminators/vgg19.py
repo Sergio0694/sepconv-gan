b0b7d2746077972b077531d6e120cc8f6ae8cdc7
Sergio0694 <sergio0694@live.com>
2018-07-12 00:03:09 +0200

    Fixed VGG19 discriminator

--- training/networks/discriminators/vgg19.py
+++ training/networks/discriminators/vgg19.py
@@ -7,9 +7,13 @@ def get_network(x):
     '''
 
     with tf.variable_scope('VGG19_top', None, [x], reuse=tf.AUTO_REUSE):
-        d1 = tf.layers.dense(x, 4096, activation=tf.nn.leaky_relu)
-        dropout1 = tf.layers.dropout(d1, 0.9)
+        conv1 = tf.layers.conv2d(x, 512, 3, activation=tf.nn.leaky_relu, padding='same')
+        conv2 = tf.layers.conv2d(conv1, 512, 3, activation=tf.nn.leaky_relu, padding='same')
+        pool = tf.layers.max_pooling2d(conv2, 3, 2, padding='valid')
+        flat = tf.reshape(pool, [pool.shape[0], -1])
+        d1 = tf.layers.dense(flat, 2048, activation=tf.nn.leaky_relu)
+        dropout1 = tf.layers.dropout(d1, 0.8)
         d2 = tf.layers.dense(dropout1, 2048, activation=tf.nn.leaky_relu)
-        dropout2 = tf.layers.dropout(d2, 0.9)
+        dropout2 = tf.layers.dropout(d2, 0.8)
         d3 = tf.layers.dense(dropout2, 1)
         return d3

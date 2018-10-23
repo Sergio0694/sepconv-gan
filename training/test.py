3dccfd94f91aa538ae81c9d9f5c71d57c7aef464
Sergio Pedri <sergio0694@live.com>
2018-10-23 16:01:04 +0200

    Added path selection in train.py script

--- training/test.py
+++ training/test.py
@@ -1,14 +1,15 @@
+import argparse
+import os
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
-from __MACRO__ import TRAINING_DATASET_PATH
 import dataset.dataset_loader as data_loader
 
-def show_samples(size):
+def show_samples(path, size):
 
     # pipeline setup
-    train_dataset = data_loader.load_train(TRAINING_DATASET_PATH, size)
+    train_dataset = data_loader.load_train(path, size)
     gen_iterator = train_dataset.make_one_shot_iterator()
     x_train, y = gen_iterator.get_next()
 
@@ -35,4 +36,14 @@ def show_samples(size):
         plt.show()
 
 if __name__ == '__main__':
-    show_samples(18)
+
+    # get arguments
+    parser = argparse.ArgumentParser(description='Shows a preview of the current training dataset.')
+    parser.add_argument('-source',help='The source folder that contains the training samples.', required=True)
+    args = vars(parser.parse_args())
+
+    # validate
+    if not os.path.isdir(args['source']):
+        raise ValueError('The source path is not valid')
+
+    show_samples(args['source'], 18)
